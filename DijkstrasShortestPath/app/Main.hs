967051566f040cbaa0c75765844c75031f1a8024
Dirk <93925356+itsDirk@users.noreply.github.com>
2024-03-03 23:40:33 +0100

    Created FizzBuzz
    😁

--- DijkstrasShortestPath/app/Main.hs
+++ DijkstrasShortestPath/app/Main.hs
@@ -3,4 +3,11 @@ module Main (main) where
 import Lib
 
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = mapM_ (putStrLn . fizzBuzz) [1..100]
+
+fizzBuzz :: Int -> String
+fizzBuzz n
+    | n `mod` 15 == 0 = "FizzBuzz"
+    | n `mod` 3 == 0 = "Fizz"
+    | n `mod` 5 == 0 = "Buzz"
+    | otherwise = show n
