5a7b2ab625938a50d197a91713933f7832ab87b2
Dirk <93925356+itsDirk@users.noreply.github.com>
2024-03-06 00:19:41 +0100

    Programmeerde quicksort!
    Implementeerde quicksort! (～￣▽￣)～

--- DijkstrasShortestPath/app/Main.hs
+++ DijkstrasShortestPath/app/Main.hs
@@ -1,14 +1,24 @@
 module Main (main) where
-
 import Lib
 
 main :: IO ()
-main =  do
-  putStrLn "Enter a number: "
-  number <- getLine
-  let result = factorial (read number :: Int)
-  putStrLn ("The factorial of " ++ number ++ " = " ++ show result)
+main = do
+    let unsortedList = [9, 3, 7, 2, 1, 1, 8, 4, 6, 5, 0, 3, 2, 7, 8, 9, 5, 4, 6, 0]
+    let sortedList = quicksort unsortedList
 
-factorial :: Int -> Int
-factorial 0 = 1
-factorial n = n * factorial (n - 1)
+    putStrLn "Unsorted list: "
+    print unsortedList
+
+    putStrLn "Sorted list: "
+    print sortedList
+
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+-- Deel de lijst op in een pivot (eerste element/head) en de rest van de lijst (de tail)
+quicksort (pivot:remaining) =
+    -- Sorteer recursief alle elementen voor de pivot
+    let smallerSorted = quicksort [element | element <- remaining, element <= pivot]
+    -- Sorteer recursief alle elementen na de pivot
+        largerSorted = quicksort [element | element <- remaining, element > pivot]
+    -- Voeg de gesorteerde lijsten en pivot samen
+    in smallerSorted ++ [pivot] ++ largerSorted
