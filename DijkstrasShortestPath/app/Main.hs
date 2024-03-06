2d81ef6aa67df26999fee5d5e7994a4ef687748b
Dirk <93925356+itsDirk@users.noreply.github.com>
2024-03-07 00:29:57 +0100

    Maakte een dobbelsteen programma
    Leerde gebruik te maken van libraries door te experimenteren met System.Random＼（〇_ｏ）／

--- DijkstrasShortestPath/app/Main.hs
+++ DijkstrasShortestPath/app/Main.hs
@@ -1,24 +1,15 @@
-module Main (main) where
-import Lib
+import System.Random (randomRIO)
 
 main :: IO ()
 main = do
-    let unsortedList = [9, 3, 7, 2, 1, 1, 8, 4, 6, 5, 0, 3, 2, 7, 8, 9, 5, 4, 6, 0]
-    let sortedList = quicksort unsortedList
+    putStrLn "Enter the size of your die:"
+    dieSizeStr <- getLine
+    let dieSize = read dieSizeStr :: Int
 
-    putStrLn "Unsorted list: "
-    print unsortedList
+    putStrLn $"Press Enter to roll the " ++ show dieSize ++ "-sided die."
+    _ <- getLine
+    roll <- rollDie dieSize
+    putStrLn $"You rolled: " ++ show roll ++ "!"
 
-    putStrLn "Sorted list: "
-    print sortedList
-
-quicksort :: Ord a => [a] -> [a]
-quicksort [] = []
--- Deel de lijst op in een pivot (eerste element/head) en de rest van de lijst (de tail)
-quicksort (pivot:remaining) =
-    -- Sorteer recursief alle elementen voor de pivot
-    let smallerSorted = quicksort [element | element <- remaining, element <= pivot]
-    -- Sorteer recursief alle elementen na de pivot
-        largerSorted = quicksort [element | element <- remaining, element > pivot]
-    -- Voeg de gesorteerde lijsten en pivot samen
-    in smallerSorted ++ [pivot] ++ largerSorted
+rollDie :: Int -> IO Int
+rollDie dieSize = randomRIO (1, dieSize)
